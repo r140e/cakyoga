77ec3a6e21a3c5a396228386750c1e3d2cc98f09
r140e <eko.rostian@gmail.com>
2020-06-13 12:40:10 +0700

    delete wordpress

---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Cakyoga') }}</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/css/uikit.min.css">
+	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">    
+
+</head>
+<body>
+	@include('layouts.partials.navblog')
+        @yield('content')
+	@include('layouts.partials.footer')
+	@include('layouts.partials.modalnav')
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/js/uikit.min.js" type="text/javascript"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/js/uikit-icons.min.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ resources/views/layouts/partials/navblog.blade.php
@@ -0,0 +1,15 @@
+<div class="uk-background-primary">
+<nav class="uk-navbar uk-container uk-light" uk-navbar="mode: click">
+    <div class="uk-navbar-left">
+        <a class="uk-navbar-item uk-logo" href="{{ url('/') }}">Cakyoga</a>
+    </div>
+    <div class="uk-navbar-right">
+        <ul class="uk-navbar-nav uk-visible@s">
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/social">Social</a></li>
+            <li><a href="/gis">GIS</a></li>
+        </ul>
+        <a class="uk-navbar-toggle uk-hidden@s" href="#offcanvas-slide" uk-navbar-toggle-icon="" uk-toggle=""></a>
+    </div>
+</nav>
+</div>
